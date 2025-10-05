add0cc4451bf77762e50fef6d8010015f18bed19
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-10-05 16:02:48 +0600

    folder-based route --> page + layout

---
+++ music-academy/src/app/chai/lemon/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LemonChaiPage = () => {
+  return (
+    <div>
+      <h2 className="text-3xl text-center">Lemon Chai</h2>
+    </div>
+  )
+}
+
+export default LemonChaiPage
---
+++ music-academy/src/app/chai/page.tsx
@@ -0,0 +1,10 @@
+
+const ChaiPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center">Chai Page</h1>
+    </div>
+  )
+}
+
+export default ChaiPage
