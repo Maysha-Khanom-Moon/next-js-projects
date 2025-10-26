af35d532a13a91f70cea64e63531afaf3ee91643
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-10-26 19:16:46 +0600

    server-side and client-side component and rendering

---
+++ again_final-mvp/app/components/hello.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Hello = () => {
+  console.log('I am a Client component');
+  
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Hello
--- again_final-mvp/app/page.tsx
+++ again_final-mvp/app/page.tsx
@@ -1,8 +1,12 @@
+import Hello from "@/app/components/hello";
 
 export default function Home() {
+  console.log('What type of a component am I?');
+
   return (
-    <div className="text-5xl underline">
-      Welcome to Next.js!
-    </div>
+    <main>
+      <div className="text-5xl underline">Welcome to Next.js!</div>
+      <Hello />
+    </main>
   );
 }
